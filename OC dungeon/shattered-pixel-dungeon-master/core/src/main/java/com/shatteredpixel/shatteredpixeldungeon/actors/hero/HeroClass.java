ee254f941b47a8a64ab4bedab2778c7caee8ebc9
Newmoonhana <59281026+Newmoonhana@users.noreply.github.com>
2022-04-25 11:25:18 +0900

    OC 던전 백업
    신규 캐릭터 포리마 구현 중

---
+++ OC dungeon/shattered-pixel-dungeon-master/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/HeroClass.java
@@ -0,0 +1,382 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2022 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.shatteredpixel.shatteredpixeldungeon.actors.hero;
+
+import com.shatteredpixel.shatteredpixeldungeon.Assets;
+import com.shatteredpixel.shatteredpixeldungeon.Badges;
+import com.shatteredpixel.shatteredpixeldungeon.Challenges;
+import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
+import com.shatteredpixel.shatteredpixeldungeon.QuickSlot;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.ArmorAbility;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.huntress.NaturesPower;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.huntress.SpiritHawk;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.huntress.SpectralBlades;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.mage.WildMagic;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.mage.WarpBeacon;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.mage.ElementalBlast;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.porrima.DeathMark_porrima;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.porrima.ShadowClone_porrima;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.porrima.SmokeBomb_porrima;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.rogue.DeathMark;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.rogue.ShadowClone;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.rogue.SmokeBomb;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.warrior.HeroicLeap;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.warrior.Shockwave;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.warrior.Endure;
+import com.shatteredpixel.shatteredpixeldungeon.items.BrokenSeal;
+import com.shatteredpixel.shatteredpixeldungeon.items.Item;
+import com.shatteredpixel.shatteredpixeldungeon.items.KingsCrown;
+import com.shatteredpixel.shatteredpixeldungeon.items.Waterskin;
+import com.shatteredpixel.shatteredpixeldungeon.items.armor.ClothArmor;
+import com.shatteredpixel.shatteredpixeldungeon.items.artifacts.CloakOfShadows;
+import com.shatteredpixel.shatteredpixeldungeon.items.artifacts.Cloaking;
+import com.shatteredpixel.shatteredpixeldungeon.items.bags.VelvetPouch;
+import com.shatteredpixel.shatteredpixeldungeon.items.food.Food;
+import com.shatteredpixel.shatteredpixeldungeon.items.potions.PotionOfHealing;
+import com.shatteredpixel.shatteredpixeldungeon.items.potions.PotionOfInvisibility;
+import com.shatteredpixel.shatteredpixeldungeon.items.potions.PotionOfLiquidFlame;
+import com.shatteredpixel.shatteredpixeldungeon.items.potions.PotionOfMindVision;
+import com.shatteredpixel.shatteredpixeldungeon.items.rings.Ring;
+import com.shatteredpixel.shatteredpixeldungeon.items.rings.RingOfWealth;
+import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.ScrollOfIdentify;
+import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.ScrollOfLullaby;
+import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.ScrollOfMagicMapping;
+import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.ScrollOfRage;
+import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.ScrollOfUpgrade;
+import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.exotic.ScrollOfDivination;
+import com.shatteredpixel.shatteredpixeldungeon.items.wands.WandOfMagicMissile;
+import com.shatteredpixel.shatteredpixeldungeon.items.weapon.SpiritBow;
+import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.Dagger;
+import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.Gloves;
+import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.MagesStaff;
+import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.WornShortsword;
+import com.shatteredpixel.shatteredpixeldungeon.items.weapon.missiles.Bolas;
+import com.shatteredpixel.shatteredpixeldungeon.items.weapon.missiles.ThrowingKnife;
+import com.shatteredpixel.shatteredpixeldungeon.items.weapon.missiles.ThrowingStone;
+import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
+import com.watabou.utils.DeviceCompat;
+
+public enum HeroClass {
+	// 직업
+	PORRIMA( HeroSubClass.ELDORADO, HeroSubClass.MIMIC ), // 직업 - 포리마
+	WARRIOR( HeroSubClass.BERSERKER, HeroSubClass.GLADIATOR ),
+	MAGE( HeroSubClass.BATTLEMAGE, HeroSubClass.WARLOCK ),
+	ROGUE( HeroSubClass.ASSASSIN, HeroSubClass.FREERUNNER ),
+	HUNTRESS( HeroSubClass.SNIPER, HeroSubClass.WARDEN );
+
+	private HeroSubClass[] subClasses;
+
+	HeroClass( HeroSubClass...subClasses ) {
+		this.subClasses = subClasses;
+	}
+
+	//플레이어 생성자
+	public void initHero( Hero hero ) {
+
+		hero.heroClass = this;
+		Talent.initClassTalents(hero);
+
+		Item i = new ClothArmor().identify();
+		if (!Challenges.isItemBlocked(i)) hero.belongings.armor = (ClothArmor)i;
+
+		i = new Food();
+		if (!Challenges.isItemBlocked(i)) i.collect();
+
+		new VelvetPouch().collect();
+		Dungeon.LimitedDrops.VELVET_POUCH.drop();
+
+		Waterskin waterskin = new Waterskin();
+		waterskin.collect();
+
+		new ScrollOfIdentify().identify();
+
+		//직업 생성자 삽입
+		switch (this) {
+			case PORRIMA:	// 직업 - 포리마
+				initPorrima( hero );
+				break;
+
+			case WARRIOR:
+				initWarrior( hero );
+				break;
+
+			case MAGE:
+				initMage( hero );
+				break;
+
+			case ROGUE:
+				initRogue( hero );
+				break;
+
+			case HUNTRESS:
+				initHuntress( hero );
+				break;
+		}
+
+		for (int s = 0; s < QuickSlot.SIZE; s++){
+			if (Dungeon.quickslot.getItem(s) == null){
+				Dungeon.quickslot.setSlot(s, waterskin);
+				break;
+			}
+		}
+
+	}
+
+	public Badges.Badge masteryBadge() {
+		switch (this) {
+			case WARRIOR:
+				return Badges.Badge.MASTERY_WARRIOR;
+			case MAGE:
+				return Badges.Badge.MASTERY_MAGE;
+			case ROGUE:
+				return Badges.Badge.MASTERY_ROGUE;
+			case HUNTRESS:
+				return Badges.Badge.MASTERY_HUNTRESS;
+		}
+		return null;
+	}
+
+	private static void initPorrima( Hero hero ) {	// 직업 - 포리마 생성자
+		(hero.belongings.weapon = new Dagger()).identify();
+		(hero.belongings.ring = new RingOfWealth()).identify();
+
+		// 특수 능력 클로킹을 장착하고 시작(원본 특별 유물 그림자의 망토(CloakOfShadows))
+		Cloaking cloak = new Cloaking();
+		(hero.belongings.artifact = cloak).identify();
+		hero.belongings.artifact.activate( hero );
+
+		// 마비용 권총 얻고 시작([DEBUG] 올가미(Bolas)로 임시 대체)
+		Bolas handgun = new Bolas();
+		handgun.quantity(1).collect();
+
+		//부유의 반지(Ring of Wealth) 하나 장착하고 시작
+		RingOfWealth ring = new RingOfWealth();
+		(hero.belongings.ring = ring).identify();
+		hero.belongings.ring.activate( hero );
+
+		Dungeon.quickslot.setSlot(0, cloak);
+		Dungeon.quickslot.setSlot(1, handgun);
+
+		Dungeon.gold = 1000;
+
+		new ScrollOfMagicMapping().identify();
+		new PotionOfInvisibility().identify();
+	}
+
+	private static void initWarrior( Hero hero ) {
+		(hero.belongings.weapon = new WornShortsword()).identify();
+		ThrowingStone stones = new ThrowingStone();
+		stones.quantity(3).collect();
+		Dungeon.quickslot.setSlot(0, stones);
+
+		if (hero.belongings.armor != null){
+			hero.belongings.armor.affixSeal(new BrokenSeal());
+		}
+
+		new PotionOfHealing().identify();
+		new ScrollOfRage().identify();
+	}
+
+	private static void initMage( Hero hero ) {
+		MagesStaff staff;
+
+		staff = new MagesStaff(new WandOfMagicMissile());
+
+		(hero.belongings.weapon = staff).identify();
+		hero.belongings.weapon.activate(hero);
+
+		Dungeon.quickslot.setSlot(0, staff);
+
+		new ScrollOfUpgrade().identify();
+		new PotionOfLiquidFlame().identify();
+	}
+
+	private static void initRogue( Hero hero ) {
+		(hero.belongings.weapon = new Dagger()).identify();
+
+		CloakOfShadows cloak = new CloakOfShadows();
+		(hero.belongings.artifact = cloak).identify();
+		hero.belongings.artifact.activate( hero );
+
+		ThrowingKnife knives = new ThrowingKnife();
+		knives.quantity(3).collect();
+
+		Dungeon.quickslot.setSlot(0, cloak);
+		Dungeon.quickslot.setSlot(1, knives);
+
+		new ScrollOfMagicMapping().identify();
+		new PotionOfInvisibility().identify();
+	}
+
+	private static void initHuntress( Hero hero ) {
+
+		(hero.belongings.weapon = new Gloves()).identify();
+		SpiritBow bow = new SpiritBow();
+		bow.identify().collect();
+
+		Dungeon.quickslot.setSlot(0, bow);
+
+		new PotionOfMindVision().identify();
+		new ScrollOfLullaby().identify();
+	}
+
+	public String title() {
+		return Messages.get(HeroClass.class, name());
+	}
+
+	public String desc(){
+		return Messages.get(HeroClass.class, name()+"_desc");
+	}
+
+	public HeroSubClass[] subClasses() {
+		return subClasses;
+	}
+
+	//[DEBUG] 뭔지 확인 필요(아마 착용 가능 방어구?)
+	public ArmorAbility[] armorAbilities(){
+		switch (this) {
+			case PORRIMA: // 직업 - 포리마
+				return new ArmorAbility[]{new SmokeBomb_porrima(), new DeathMark_porrima(), new ShadowClone_porrima()};
+			case WARRIOR: default:
+				return new ArmorAbility[]{new HeroicLeap(), new Shockwave(), new Endure()};
+			case MAGE:
+				return new ArmorAbility[]{new ElementalBlast(), new WildMagic(), new WarpBeacon()};
+			case ROGUE:
+				return new ArmorAbility[]{new SmokeBomb(), new DeathMark(), new ShadowClone()};
+			case HUNTRESS:
+				return new ArmorAbility[]{new SpectralBlades(), new NaturesPower(), new SpiritHawk()};
+		}
+	}
+
+	// 캐릭터 스프라이트 시트
+	public String spritesheet() {
+		switch (this) {
+			case PORRIMA: // 직업 - 포리마
+				return Assets.Sprites.PORRIMA;
+			case WARRIOR: default:
+				return Assets.Sprites.WARRIOR;
+			case MAGE:
+				return Assets.Sprites.MAGE;
+			case ROGUE:
+				return Assets.Sprites.ROGUE;
+			case HUNTRESS:
+				return Assets.Sprites.HUNTRESS;
+		}
+	}
+
+	//캐릭터 스탠딩 일러
+	public String splashArt(){
+		switch (this) {
+			case PORRIMA: // 직업 - 포리마
+				return Assets.Splashes.PORRIMA;
+			case WARRIOR: default:
+				return Assets.Splashes.WARRIOR;
+			case MAGE:
+				return Assets.Splashes.MAGE;
+			case ROGUE:
+				return Assets.Splashes.ROGUE;
+			case HUNTRESS:
+				return Assets.Splashes.HUNTRESS;
+		}
+	}
+
+	//[DEBUG] 뭔지 확인 필요(캐릭터 레벨에 따른 능력 스탯 찍는거 관련인듯?)
+	public String[] perks() {
+		switch (this) {
+			case PORRIMA: // 직업 - 포리마
+				return new String[]{
+						Messages.get(HeroClass.class, "porrima_perk1"),
+						Messages.get(HeroClass.class, "porrima_perk2"),
+						Messages.get(HeroClass.class, "porrima_perk3"),
+						Messages.get(HeroClass.class, "porrima_perk4"),
+						Messages.get(HeroClass.class, "porrima_perk5"),
+				};
+			case WARRIOR: default:
+				return new String[]{
+						Messages.get(HeroClass.class, "warrior_perk1"),
+						Messages.get(HeroClass.class, "warrior_perk2"),
+						Messages.get(HeroClass.class, "warrior_perk3"),
+						Messages.get(HeroClass.class, "warrior_perk4"),
+						Messages.get(HeroClass.class, "warrior_perk5"),
+				};
+			case MAGE:
+				return new String[]{
+						Messages.get(HeroClass.class, "mage_perk1"),
+						Messages.get(HeroClass.class, "mage_perk2"),
+						Messages.get(HeroClass.class, "mage_perk3"),
+						Messages.get(HeroClass.class, "mage_perk4"),
+						Messages.get(HeroClass.class, "mage_perk5"),
+				};
+			case ROGUE:
+				return new String[]{
+						Messages.get(HeroClass.class, "rogue_perk1"),
+						Messages.get(HeroClass.class, "rogue_perk2"),
+						Messages.get(HeroClass.class, "rogue_perk3"),
+						Messages.get(HeroClass.class, "rogue_perk4"),
+						Messages.get(HeroClass.class, "rogue_perk5"),
+				};
+			case HUNTRESS:
+				return new String[]{
+						Messages.get(HeroClass.class, "huntress_perk1"),
+						Messages.get(HeroClass.class, "huntress_perk2"),
+						Messages.get(HeroClass.class, "huntress_perk3"),
+						Messages.get(HeroClass.class, "huntress_perk4"),
+						Messages.get(HeroClass.class, "huntress_perk5"),
+				};
+		}
+	}
+
+	public boolean isUnlocked(){
+		//always unlock on debug builds
+		if (DeviceCompat.isDebug()) return true;	//디버그 상태 시 모든 캐릭터 자동 해금(본 게임에선 정상 락)
+		
+		switch (this){
+			case PORRIMA:
+				return Badges.isUnlocked(Badges.Badge.UNLOCK_PORRIMA);
+			case WARRIOR: default:
+				return true;
+			case MAGE:
+				return Badges.isUnlocked(Badges.Badge.UNLOCK_MAGE);
+			case ROGUE:
+				return Badges.isUnlocked(Badges.Badge.UNLOCK_ROGUE);
+			case HUNTRESS:
+				return Badges.isUnlocked(Badges.Badge.UNLOCK_HUNTRESS);
+		}
+	}
+
+	//언락되지 않은 캐릭터 표시 메세지
+	public String unlockMsg() {
+		switch (this){
+			case PORRIMA: // 직업 - 포리마
+				return Messages.get(HeroClass.class, "porrima_unlock");
+			case WARRIOR: default:
+				return "";
+			case MAGE:
+				return Messages.get(HeroClass.class, "mage_unlock");
+			case ROGUE:
+				return Messages.get(HeroClass.class, "rogue_unlock");
+			case HUNTRESS:
+				return Messages.get(HeroClass.class, "huntress_unlock");
+		}
+	}
+
+}
